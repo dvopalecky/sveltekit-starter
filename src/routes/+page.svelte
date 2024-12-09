9910ce1fa21eb61bb5f5624e72d577a147777318
Daniel Vopalecky <daniel.vopalecky@quarisma.fr>
2024-12-09 21:28:05 +0100

    Use Daisy UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
-<h1 class="text-4xl font-bold text-gray-800 mb-4">Welcome to SvelteKit</h1>
-<p class="text-lg text-gray-600">
-  Visit <a
-    href="https://svelte.dev/docs/kit"
-    class="text-blue-600 hover:text-blue-800 underline">svelte.dev/docs/kit</a
-  > to read the documentation
-</p>
+<div class="min-h-screen flex flex-col items-center justify-center">
+  <div class="text-center">
+    <h1 class="text-5xl font-bold mb-4">Welcome to SvelteKit</h1>
+    <p class="text-lg">
+      Visit <a href="https://svelte.dev/docs/kit" class="link link-primary"
+        >svelte.dev/docs/kit</a
+      > to read the documentation
+    </p>
+  </div>
+</div>
